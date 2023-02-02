ff65a8ad1ba0abe573a8654ced393bc312664fc5
Knowledge Ngwarayi <tkngwarayi@gmail.com>
2023-02-02 15:00:44 +0200

    Area and services

---
+++ src/main/area/service/AreaService.java
@@ -0,0 +1,19 @@
+package zw.co.econet.assessment.area.service;
+
+import zw.co.econet.assessment.area.Area;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AreaService {
+
+    Optional<Area> save(Area area);
+
+
+    List<Area> findAll();
+
+    Optional<Area> findById(Long id);
+
+
+
+}
